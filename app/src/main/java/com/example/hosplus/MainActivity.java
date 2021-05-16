9601c88c453168ad310680f9c69a7b4a7e72d9f1
Kartik Deepak Dange <kartikdange2002@gmail.com>
2021-05-16 16:45:48 +0530

    Created a basic app layout

--- app/src/main/java/com/example/hosplus/MainActivity.java
+++ app/src/main/java/com/example/hosplus/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.hosplus;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    CardView doctorCard, aboutCard, locationCard, scheduleCard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //creating object of cards
+
+        doctorCard = findViewById(R.id.doctorCard);
+        scheduleCard = findViewById(R.id.ScheduleCard);
+        locationCard = findViewById(R.id.locationCard);
+        aboutCard = findViewById(R.id.aboutCard);
+
+        //creating intent for each card
+
+        doctorCard.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), DoctorsList.class);
+            startActivity(intent);
+        });
+        scheduleCard.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), Schedule.class);
+            startActivity(intent);
+        });
+        locationCard.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), Location.class);
+            startActivity(intent);
+        });
+        aboutCard.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), AboutUs.class);
+            startActivity(intent);
+        });
+
     }
 }
