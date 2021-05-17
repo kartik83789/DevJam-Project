8158b35b79291bcaa1961c30eafe495a6bce741a
Kartik Deepak Dange <kartikdange2002@gmail.com>
2021-05-17 15:27:21 +0530

    Made basic map layout, added map API

--- app/src/main/java/com/example/hosplus/Location.java
+++ app/src/main/java/com/example/hosplus/Location.java
@@ -1,14 +1,36 @@
 package com.example.hosplus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Location extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Location extends AppCompatActivity implements OnMapReadyCallback {
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng Hospital = new LatLng(29.514836, 76.907477);
+        map.addMarker(new MarkerOptions().position((Hospital)).title("Hospital"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Hospital));
     }
 }
