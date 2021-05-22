441affdb5351a8ee19d412893223c14d65f1c913
Kartik Deepak Dange <kartikdange2002@gmail.com>
2021-05-22 13:02:56 +0530

    Completed ambulance

--- app/src/main/java/com/example/hosplus/Ambulance.java
+++ app/src/main/java/com/example/hosplus/Ambulance.java
@@ -2,13 +2,24 @@ package com.example.hosplus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Ambulance extends AppCompatActivity {
-
+    Button call;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ambulance);
+
+        call = (Button)findViewById(R.id.callButton);
+        String number = "1221345214";
+        call.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + number));
+            startActivity(intent);
+        });
+
     }
 }
