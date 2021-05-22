fe4b930432fe2a60f4fdff8c9eae9c86e7071280
Kartik Deepak Dange <kartikdange2002@gmail.com>
2021-05-22 14:22:37 +0530

    Completed remaining features

--- app/src/main/java/com/example/hosplus/Ambulance.java
+++ app/src/main/java/com/example/hosplus/Ambulance.java
@@ -1,11 +1,16 @@
 package com.example.hosplus;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Ambulance extends AppCompatActivity {
     Button call;
@@ -14,6 +19,18 @@ public class Ambulance extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ambulance);
 
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.CALL_PHONE) ==
+                PackageManager.PERMISSION_GRANTED) {
+            // You can use the API that requires the permission.
+            Toast.makeText(this, "Permission is granted", Toast.LENGTH_LONG).show();
+        } else {
+            // You can directly ask for the permission.
+            // The registered ActivityResultCallback gets the result of this request.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE},
+                    1000);
+        }
+
         call = (Button)findViewById(R.id.callButton);
         String number = "1221345214";
         call.setOnClickListener(v -> {
@@ -21,5 +38,18 @@ public class Ambulance extends AppCompatActivity {
             startActivity(intent);
         });
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1000) {
+            if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission granted, can make a call.", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Permission NOT granted, cannot make a call.", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
     }
 }
